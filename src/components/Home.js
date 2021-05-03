816d114f524c2528e3be3ef7d4199b50e4f7d030
Adam Venord <AdamVenord7@gmail.com>
2021-05-03 14:25:37 -0600

    added cards on the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Card, Image } from 'semantic-ui-react'
 
 const Home = () => (
-  <h1>Home</h1>
-);
+  <>
+    <h1>Home</h1>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    </Card>
+  </>
+)
 
 export default Home;
