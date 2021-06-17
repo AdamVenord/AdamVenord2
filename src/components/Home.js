01b04561d83e972cbaf312fc1bb6084f487feafd
Adam Venord <AdamVenord7@gmail.com>
2021-06-17 14:28:27 -0600

    correctly linked the photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,43 @@
 import React from "react";
 import { Card, Image } from 'semantic-ui-react'
+import awards from './Images/awards.png'
+import coding from './Images/coding.png'
+import contact from './Images/contact.png'
+import files from './Images/files.png'
+import longboarding from './Images/longboarding.png'
+import music from './Images/music.png'
+import polaroid from './Images/polaroid.png'
 
 const Home = () => (
   <>
     <h1>Home</h1>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* About me */}
+      <Image src={longboarding} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* awards */}
+      <Image src={awards} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* files */}
+      <Image src={files} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* music */}
+      <Image src={music} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* polaroid */}
+      <Image src={polaroid} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* contact me OwO */}
+      <Image src={contact} />
     </Card>
     <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-    </Card>
-    <Card>
-      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      {/* projects and more */}
+      <Image src={coding} />
     </Card>
   </>
 )
